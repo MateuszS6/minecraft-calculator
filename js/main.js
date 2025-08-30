1cceadb27d40966d43e9d4027ad3fe86adcab648
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2025-08-30 23:38:40 +0100

    Re-implement custom diamond sword cursor
    Moved cursor image assets to a new directory and updated references in CSS. Added a custom cursor element and JavaScript logic to display and transform the cursor based on mouse position, replacing the default system cursor. Disabled native cursor styles for inputs, buttons, and links to ensure consistent custom cursor behavior.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,48 @@ import {initPathModule} from "./path.js";
 
 initConversionModule();
 initPathModule();
+
+/* CURSOR */
+
+const cursor = document.getElementById("cursor");
+const cursorSize = 64;
+
+document.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    let transform = "rotate(0deg)";
+    let offsetX = 0;
+    let offsetY = 0;
+
+    // Right
+    if (x > window.innerWidth - cursorSize) {
+        transform = "scaleX(-1)";
+        offsetX = cursorSize;
+    }
+
+    // Bottom
+    if (y > window.innerHeight - cursorSize) {
+        transform = "scaleY(-1)"
+        offsetY = cursorSize;
+    }
+
+    // Bottom right
+    if (x > window.innerWidth - cursorSize && y > window.innerHeight - cursorSize) {
+        transform = "scale(-1, -1)"
+        offsetX = cursorSize;
+        offsetY = cursorSize;
+    }
+
+    cursor.style.transform = transform;
+    cursor.style.left = (x - offsetX) + "px";
+    cursor.style.top = (y - offsetY) + "px";
+});
+
+document.addEventListener("mouseleave", () => {
+    cursor.style.opacity = "0";
+});
+
+document.addEventListener("mouseenter", () => {
+    cursor.style.opacity = "1";
+});
